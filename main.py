8cbdaabed3d9100c89dd08d8291b91b72d57daa0
nicofarr <nicofarr09@gmail.com>
2026-04-25 23:09:48 +0200

    test run

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
 from xml.parsers.expat import model
-
+import numpy as np
 
 def main():
     print("Test run")
     print("Downloading YouTube short and processing it with TribeModel...")
     import subprocess
+    import pandas as pd
 
     # Download the YouTube short
-    url = "https://www.youtube.com/shorts/3YxtTAMNJhM"
+    url = "https://www.youtube.com/watch?v=uIxxyiiddgU"
     output_path = "video.mp4"
     subprocess.run(["yt-dlp", "-o", output_path, url], check=True)
 
     from tribev2 import TribeModel
 
-    model = TribeModel.from_pretrained("facebook/tribev2", cache_folder="./cache")
+    model = TribeModel.from_pretrained("facebook/tribev2", cache_folder="/Brain/private/nfarrugi/",device="cuda")
 
-    df = model.get_events_dataframe(audio_path="video.wav")
+    df = model.get_events_dataframe(video_path="video.mp4")
+    df.to_csv('events.csv')
+    #df = pd.read_csv('events.csv')
     print(df.head())
-    #preds, segments = model.predict(events=df)
-    # print(preds.shape)  # (n_timesteps, n_vertices)
+    preds, segments = model.predict(events=df)
+    print(preds.shape)  # (n_timesteps, n_vertices)
+
+    # save the predictions
+    np.savez_compressed('testpreds.npz',preds=preds)
 
 
 if __name__ == "__main__":
