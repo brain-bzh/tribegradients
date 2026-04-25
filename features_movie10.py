4e7b1a94a173c5605735fbb984cdc772cb8b3b75
Nicolas Farrugia <nicolas.farrugia@imt-atlantique.fr>
2026-04-25 23:35:04 +0200

    added arguments

--- features_movie10.py
+++ features_movie10.py
@@ -3,6 +3,13 @@ from neuralset.events import transforms
 import numpy as np 
 from tribev2 import TribeModel
 from tribev2.demo_utils import get_audio_and_text_events
+import argparse
+import os 
+
+parser = argparse.ArgumentParser(description='Generate predictions for Algonauts2025 movie data')
+parser.add_argument('--movie', type=str, default='bourne', help='Movie name to process')
+parser.add_argument('--output', type=str, default='./', help='Output path for predictions')
+args = parser.parse_args()
 
 study = Algonauts2025(path="/Brain/public/datasets/neuromod/",query = "task in ['movie10'] and subject == 'Algonauts2025/sub-01' ",infra={"backend": "Cached", "folder": "/Brain/public/datasets/neuromod/cache"},infra_timelines={"cluster": None}) ## the code then looks for path + 'download/algonauts_2025.competitors' 
 
@@ -11,7 +18,8 @@ print(summary[["subject", "timeline"]].to_string())
 
 events = study.run()
 
-query = transforms.QueryEvents(query='movie == "bourne" ')
+movie_name = args.movie
+query = transforms.QueryEvents(query=f'movie == "{movie_name}" ')
 events = query(events)
 
 query = transforms.QueryEvents(query='type != "Fmri" ')
@@ -25,5 +33,6 @@ events_forinference = get_audio_and_text_events(events)
 
 model = TribeModel.from_pretrained("facebook/tribev2", cache_folder="/Brain/private/nfarrugi/",device="cuda")
 
-preds = model.predict(events_forinference)
-np.savez_compressed('testpreds.npz',preds=preds)
+preds,segments = model.predict(events_forinference)
+
+np.savez_compressed(os.path.join(args.output, f'{args.movie}_predictions.npz'), preds=preds, segments=segments)
